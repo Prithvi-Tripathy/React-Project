4271e7b505222ed05fe8a1a6b0e11a7757544312
DESKTOP-H5G6TNG\PRITHVI <prithvi.tripathy@gmail.com>
2024-09-01 23:28:14 +0530

    made css changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,43 @@
-import React,{useState, useEffect} from "react";
-import './HomeCSS.css';
-import RestroCard from "./RestroCard"
-const Home=()=>{
-  const [restroList,setRestroList]=useState([])
+import React, { useState, useEffect } from "react";
+import "./HomeCSS.css";
+import RestroCard from "./RestroCard";
+const Home = () => {
+  const [restroList, setRestroList] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     callAPI();
-  },[])
+  }, []);
 
-  const callAPI=async()=>{
-    let body = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4946106&lng=78.3875336&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    let Json = await body.json();
-    console.log("Json",Json)
-    setRestroList(Json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
-    
-  }
-
-    return (
-        <div className="container-fluid home-container">
-          <div>******************this section is to impliment filters and buttons****************************</div>
-          <button onClick={()=>{ 
-            setRestroList(restroList.filter((item)=>item.info.avgRating > 4 ))
-          //  restroList= restroList.filter((item)=>item.info.avgRating > 4 )
-           console.log("restroListJS",restroList)
-          }}></button>
-          <div className="d-flex restaurant-container">
-              {
-                restroList.map((item)=>(
-                  <RestroCard key={item.info.id} data={item.info}/>
-                ))
-              }
-            </div>
-        </div>
+  const callAPI = async () => {
+    let body = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4946106&lng=78.3875336&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
-}
+    let Json = await body.json();
+    console.log("Json", Json);
+    setRestroList(
+      Json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
+  return (
+    <div className="container-fluid home-container">
+      <div>
+        <button
+          onClick={() => {
+            setRestroList(restroList.filter((item) => item.info.avgRating > 4));
+            //  restroList= restroList.filter((item)=>item.info.avgRating > 4 )
+            console.log("restroListJS", restroList);
+          }}
+        >Filter</button>
+      </div>
+
+      <div className="d-flex restaurant-container">
+        {restroList.map((item) => (
+          <RestroCard key={item.info.id} data={item.info} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
